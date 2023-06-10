aefcbcccba6e0e0a2a6aecc66109c0e0d5a1b400
rendicent <133162464+rendicent@users.noreply.github.com>
2023-06-10 21:21:39 +0000

    COME ON

--- Root/script.js
+++ Root/script.js
@@ -39,3 +39,15 @@ $(document).ready(function () {
         }, ]
     });
 });
+$(function(){
+    $("a").each(function (index, element){
+        var href = $(this).attr("href");
+        $(this).attr("hiddenhref", href);
+        $(this).removeAttr("href");
+    });
+    $("a").click(function(){
+        url = $(this).attr("hiddenhref");
+       // window.open(url);
+                                  window.location.href = url;
+    })
+});
