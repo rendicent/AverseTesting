f22d015cd18c2f1caa88cecad6143175f8e75509
rendicent <133162464+rendicent@users.noreply.github.com>
2023-06-09 20:17:08 -0400

    important

--- Assets/husky.local.sh
+++
@@ -1,6 +0,0 @@
-# Created by Husky v4.3.0 (https://github.com/typicode/husky#readme)
-#   At: 5/10/2023, 10:34:46 PM
-#   From: /workspaces/ra/staking-dapp/node_modules/husky (undefined)
-
-packageManager=npm
-cd "."
--- Assets/index (3).js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <h1>This is a purple UI Next.js page</h1>;
-};
-
-export default App;
